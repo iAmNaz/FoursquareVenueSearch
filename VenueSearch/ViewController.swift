8909cc8609813d2c1ee5af2d834b912bc0d0f409
Nazario Mariano <nmariano@wylog.com>
2018-10-26 16:32:01 +0800

    Initial Commit

---
+++ VenueSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VenueSearch
+//
+//  Created by Nazario Mariano on 26/10/2018.
+//  Copyright © 2018 Nazario Mariano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VenueSearchTests/VenueSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  VenueSearchTests.swift
+//  VenueSearchTests
+//
+//  Created by Nazario Mariano on 26/10/2018.
+//  Copyright © 2018 Nazario Mariano. All rights reserved.
+//
+
+import XCTest
+@testable import VenueSearch
+
+class VenueSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
