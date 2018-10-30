24befcd7d1a6ac4e1387d1d53f364a648f7193e8
Nazario Mariano <nmariano@wylog.com>
2018-10-30 14:01:58 +0800

    Move the viewcontroller into its own group

---
+++ VenueSearch/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VenueSearch
+//
+//  Created by Nazario Mariano on 26/10/2018.
+//  Copyright © 2018 Nazario Mariano. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
