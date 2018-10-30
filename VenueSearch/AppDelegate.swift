e0d4e4c92ece4850b82ad1ae7ad2d313963ae9fa
Nazario Mariano <nmariano@wylog.com>
2018-10-30 23:27:17 +0800

    Add dependency injection registration
    The MainViewController, APIController and LocationController were registered as observers
    for incoming requests

--- VenueSearch/AppDelegate.swift
+++ VenueSearch/AppDelegate.swift
@@ -6,22 +6,46 @@
 //  Copyright © 2018 Nazario Mariano. All rights reserved.
 //
 
-/*
- Client ID
- DSXABGDSRON0OEK0YXWB3CSUNBJ5ZY4QH2LJWWR13H1M0NIR
- Client Secret
- SM1Z1N4WNVBCZ1I22XNFGOTSPFQFBOCEQ3IL00UJSISPMDWG
- */
-
 import UIKit
+import Dip
+import DecouplerKit
+import CoreLocation
+
+extension MainViewController: StoryboardInstantiatable { }
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let container = DependencyContainer { container in
+        
+        container.register(.singleton) { ResponderRegistry() as ResponderRegistry }
+        
+        container.register(storyboardType: MainViewController.self, tag: "MainViewController")
+            .resolvingProperties { container, controller in
+                let registry = try container.resolve() as ResponderRegistry
+                controller.registry = registry
+                registry.register(inputHandler: controller)
+        }
+
+        DependencyContainer.uiContainers = [container]
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let registry = try! container.resolve() as ResponderRegistry
+        let locationManager = CLLocationManager()
+        let locationController = LocationController(locationManager: locationManager)
+            locationController.uiRegistry = registry
+        
+        let apiController = APIController(session: URLSession(configuration: .default))
+            apiController.uiRegistry = registry
+            locationController.delegate = apiController
+        
+        registry.register(inputHandler: locationController, withKey: keyLocationController)
+        registry.register(inputHandler: apiController)
+        
         return true
     }
 
