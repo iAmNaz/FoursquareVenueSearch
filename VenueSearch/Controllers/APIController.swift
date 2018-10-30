a88143289c56aec0fac0857c58e6f483fbfa1fc8
Nazario Mariano <nmariano@wylog.com>
2018-10-30 13:51:24 +0800

    Add APIController and tests
    This controller will fetch venue information from the remote API and transmit to handlers

---
+++ VenueSearch/Controllers/APIController.swift
@@ -0,0 +1,144 @@
+//
+//  APIController.swift
+//  VenueSearch
+//
+//  Created by Nazario Mariano on 26/10/2018.
+//  Copyright © 2018 Nazario Mariano. All rights reserved.
+//
+
+import UIKit
+import DecouplerKit
+import PromiseKit
+
+let venueSearchEndPoint = "https://api.foursquare.com/v2/venues/search"
+let clientId = "DSXABGDSRON0OEK0YXWB3CSUNBJ5ZY4QH2LJWWR13H1M0NIR"
+let clientSecret = "SM1Z1N4WNVBCZ1I22XNFGOTSPFQFBOCEQ3IL00UJSISPMDWG"
+
+class APIController: NSObject, URLSessionDelegate {
+    var uiRegistry: ResponderRegistry!
+    
+    var reachability: Reachability!
+    
+    private var session: URLSessionProtocol!
+    
+    private var currentLocation: (latitude: Float, longitude: Float)!
+    
+    init(session: URLSessionProtocol) {
+        super.init()
+        self.reachability = Reachability()
+        self.session = session
+    }
+    
+    func reloadList(latitude: Float, longitude: Float) {
+        
+        if !reachable() {
+            markOffline()
+            return
+        }
+        
+        currentLocation = (latitude: latitude, longitude: longitude)
+        
+        let request = createVenueSearchRequest(latitude: latitude, longitude: longitude)
+        
+        self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetching)))
+        
+        let task = session.dataTask(with: request, completionHandler: {data, response, err -> Void in
+            
+            if err != nil {
+                self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetchFailed)))
+                return
+            }
+            
+            if let inData = data {
+                do {
+                    let response = try decodeJson(data: inData)
+                    self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetchCompleted)))
+                    
+                    if response.meta.code == 200 {
+                        
+                        var venuesVM = [VenueViewModel]()
+                        
+                        for venue in response.response.venues {
+                            let vm = VenueViewModel(venue: venue)
+                                venuesVM.append(vm)
+                        }
+                        self.uiRegistry.tx(request: Request(proc: Task.mainView(.displayData), body: venuesVM))
+                    } else{
+                        self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetchFailed)))
+                    }
+                } catch {
+                    print("error \(error)")
+                    self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetchFailed)))
+                }
+            }else{
+                self.uiRegistry.tx(request: Request(proc: Task.mainView(.fetchFailed)))
+            }
+        })
+        
+        task.resume()
+    }
+    
+    private func createVenueSearchRequest(latitude: Float, longitude: Float) -> URLRequest {
+        
+        let url = "\(venueSearchEndPoint)?ll=\(latitude),\(longitude)&radius=2000&client_id=\(clientId)&client_secret=\(clientSecret)&v=20181001"
+        
+        var request = URLRequest(url: URL(string: url)!)
+            request.httpMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        return request
+    }
+    
+    func startMonitoring() {
+        reachability.whenReachable = { reachability in
+            self.markOnline()
+        }
+        reachability.whenUnreachable = { _ in
+            self.markOffline()
+        }
+        
+        do {
+            try reachability.startNotifier()
+        } catch {
+            self.markOffline()
+            print("Unable to start notifier")
+        }
+    }
+    
+    func reachable() -> Bool {
+        return reachability.connection() != .none
+    }
+    
+    private func markOnline() {
+        self.uiRegistry.tx(request: Request(proc: Task.mainView(.online)))
+    }
+    
+    private func markOffline() {
+        self.uiRegistry.tx(request: Request(proc: Task.mainView(.offline)))
+    }
+}
+
+extension APIController: LocationControllerDelegate {
+    func locationChanged(latitude: Float, longitude: Float) {
+        reloadList(latitude: latitude, longitude: longitude)
+    }
+}
+
+protocol URLSessionProtocol {
+    typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
+
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping URLSessionProtocol.DataTaskResult) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
---
+++ VenueSearchTests/APIControllerTests.swift
@@ -0,0 +1,270 @@
+//
+//  APIControllerTests.swift
+//  VenueSearchTests
+//
+//  Created by Nazario Mariano on 26/10/2018.
+//  Copyright © 2018 Nazario Mariano. All rights reserved.
+//
+
+import XCTest
+import DecouplerKit
+
+@testable import VenueSearch
+
+class APIControllerTests: XCTestCase {
+
+    var session = MockURLSession()
+    var data: Data!
+    var dataFailure: Data!
+    
+    override func setUp() {
+        do {
+            self.data = try loadDataFromJSONFile(name: "venues")
+            self.dataFailure = try loadDataFromJSONFile(name: "venues-fail")
+        } catch {
+            print("Loading data error: \(error.localizedDescription)")
+        }
+    }
+
+    override func tearDown() {}
+    
+    func testSampleDataLoaded() {
+        XCTAssertNotNil(self.data)
+    }
+    
+    func testReloadListWhenReachabilityIsOfflineShouldBeMarkedOffline() {
+        let registry = MockRegistry()
+        let api = APIController(session: session)
+            api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+            reachability!.currentConn = Connection.none
+        
+            api.reachability = reachability
+        
+            api.reloadList(latitude: 0, longitude: 0)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.offline))
+    }
+    
+    func testStartMonitoringWhenUnreachableShouldBeMarkedOffline() {
+        let registry = MockRegistry()
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.none
+        
+        api.reachability = reachability
+        api.startMonitoring()
+        
+        let block = reachability?.whenUnreachable
+            block!(reachability!)
+        XCTAssert(registry.currentTask == Task.mainView(.offline))
+    }
+
+    func testStartMonitoringWhenReachableShouldBeMarkedOnline() {
+        let registry = MockRegistry()
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+            reachability!.currentConn = Connection.cellular
+        
+        api.reachability = reachability
+        api.startMonitoring()
+        
+        let block = reachability?.whenReachable
+        block!(reachability!)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.online))
+    }
+    
+    func testStartMonitoringWhenStartNotifierThrowsException() {
+        let registry = MockRegistry()
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.cellular
+        reachability!.shouldThrow = true
+        api.reachability = reachability
+        api.startMonitoring()
+        
+        XCTAssert(registry.currentTask == Task.mainView(.offline))
+    }
+
+    
+    func testReloadListWhenReachabilityIsOnlineShouldBeMarkedFetching() {
+        let registry = MockRegistry()
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.fetching))
+    }
+
+    func testReloadListWhenCalledShouldResumeDataTask() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        XCTAssert(dataTask.resumeWasCalled == true)
+    }
+    
+    func testReloadListShouldHaveCorrectUrl() {
+        let url = "\(venueSearchEndPoint)?ll=0.0,0.0&radius=2000&client_id=\(clientId)&client_secret=\(clientSecret)&v=20181001"
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+            api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        XCTAssert(session.url?.absoluteString == url)
+    }
+    
+    func testReloadListShouldFailWhenDataTaskEncountersError() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        let urlRequest = URLRequest(url: URL(string: "http://www.test.com")!)
+        
+        session.handler!(self.data, session.successHttpURLResponse(request: urlRequest), AppError.generic(.undefined(message: "Error")))
+        
+        XCTAssert(registry.currentTask == Task.mainView(.fetchFailed))
+    }
+    
+    func testReloadListShouldFailWhenDataIsNil() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        let urlRequest = URLRequest(url: URL(string: "http://www.test.com")!)
+        
+        session.handler!(nil, session.successHttpURLResponse(request: urlRequest), nil)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.fetchFailed))
+    }
+    
+    func testReloadListShouldCompleteWhenOnlineWithValidData() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        let urlRequest = URLRequest(url: URL(string: "http://www.test.com")!)
+        
+        session.handler!(self.data, session.successHttpURLResponse(request: urlRequest), nil)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.displayData))
+    }
+    
+    func testReloadListShouldDisplay10Venues() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        let urlRequest = URLRequest(url: URL(string: "http://www.test.com")!)
+        
+        session.handler!(self.data, session.successHttpURLResponse(request: urlRequest), nil)
+        let vms = registry.request.body() as [VenueViewModel]
+        XCTAssert(vms.count == 10)
+    }
+
+    
+    func testReloadListShouldFailWhenOnlineButWithHttpError() {
+        let registry = MockRegistry()
+        let dataTask = MockURLSessionDataTask()
+        session.dataTask = dataTask
+        
+        let api = APIController(session: session)
+        api.uiRegistry = registry
+        
+        let reachability = MockReachability()
+        reachability!.currentConn = Connection.wifi
+        
+        api.reachability = reachability
+        
+        api.reloadList(latitude: 0, longitude: 0)
+        
+        let urlRequest = URLRequest(url: URL(string: "http://www.test.com")!)
+        
+        session.handler!(self.dataFailure, session.successHttpURLResponse(request: urlRequest), nil)
+        
+        XCTAssert(registry.currentTask == Task.mainView(.fetchFailed))
+    }
+    
+    func testLocationDelegateImplementedShouldDisplayData() {
+        let dataTask = MockURLSessionDataTask()
+            session.dataTask = dataTask
+        
+        let api = MockAPIController(session: session)
+            api.locationChanged(latitude: 0, longitude: 0)
+        
+        XCTAssert(api.reloadCalled)
+    }
+}
